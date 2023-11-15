5aa7f1cdf1d68c12370a2887ce6da02b7a5e1c1f
esteban-florez <eflorez077@gmail.com>
2023-11-15 19:39:02 -0400

    intial page prototype

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import flowbite from 'flowbite/plugin'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite],
+}
