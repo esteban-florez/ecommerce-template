e5da8c306b8080edf04848ecc0bcb37e81ab9019
esteban-florez <eflorez077@gmail.com>
2023-11-20 21:49:58 -0400

    removed 360 image

--- src/panellum.js
+++
@@ -1,8 +0,0 @@
-import 'pannellum/build/pannellum'
-import 'pannellum/build/pannellum.css'
-
-pannellum.viewer('panorama', {
-  type: 'equirectangular',
-  panorama: '/img/360.jpg',
-  autoLoad: true,
-})
